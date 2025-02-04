04e7e7953075f1397c5356427ecf9881a36359c2
samad13 <yusufabdulsamad93@gmail.com>
2025-02-04 23:09:36 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './App.css'
+import Box from './components/Box'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Box/>
+    </>
+  )
+}
+
+export default App
