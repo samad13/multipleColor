f788e6bc4797ff68a5844bd387ebdbae95a3a53f
samad13 <yusufabdulsamad93@gmail.com>
2025-02-05 22:50:49 +0100

    bop

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: process.env.PORT || 5173,
+    host: '0.0.0.0' // Ensures it's accessible
+  },
   plugins: [react()],
 })
+
+
+
+
+
